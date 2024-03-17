7a0d51d46a044d266f36a82d6ee2ae432cddeac6
Mayel de Borniol <git@mayel.space>
2024-03-17 17:16:09 +0000

    https://github.com/bonfire-networks/bonfire-app/issues/900

--- lib/components/profile_follows/profile_follows_live.ex
+++ lib/components/profile_follows/profile_follows_live.ex
@@ -2,9 +2,9 @@ defmodule Bonfire.UI.Social.Graph.ProfileFollowsLive do
   use Bonfire.UI.Common.Web, :stateless_component
 
   prop user, :map
-  prop selected_tab, :any
-  prop feed, :list
-  prop page_info, :any
+  prop selected_tab, :any, default: nil
+  prop feed, :list, default: []
+  prop page_info, :any, default: nil
   prop showing_within, :atom, default: :profile
   prop hide_tabs, :boolean, default: false
 
