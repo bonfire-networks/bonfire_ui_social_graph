df31c6fa4fd473b1081711fba0ce9e24c71634a9
Mayel de Borniol <git@mayel.space>
2024-04-03 14:07:25 +0100

    https://github.com/bonfire-networks/bonfire-app/issues/869

--- lib/components/profile_follows/profile_follows_live.ex
+++ lib/components/profile_follows/profile_follows_live.ex
@@ -1,6 +1,15 @@
 defmodule Bonfire.UI.Social.Graph.ProfileFollowsLive do
   use Bonfire.UI.Common.Web, :stateless_component
 
+  declare_extension("UI for social graph",
+    icon: "fluent:people-community-48-filled",
+    emoji: "🫂",
+    description:
+      l(
+        "User interfaces for following users, managing aliases, importing/exporting follows, and the like."
+      )
+  )
+
   prop user, :map
   prop selected_tab, :any, default: nil
   prop feed, :list, default: []
