20ba0cdc331060d7e34c7ae394f1564e5fb9e1d6
ivan <ivanminutillo@outlook.it>
2024-12-12 11:38:48 +0100

    ui

--- lib/components/profile_follows/profile_follows_live.ex
+++ lib/components/profile_follows/profile_follows_live.ex
@@ -4,13 +4,14 @@ defmodule Bonfire.UI.Social.Graph.ProfileFollowsLive do
   declare_extension("UI for social graph",
     icon: "fluent:people-community-48-filled",
     emoji: "🫂",
+    exclude_from_nav: true,
     description:
       l(
         "User interfaces for following users, managing aliases, importing/exporting follows, and the like."
       )
   )
 
-  prop user, :map
+  prop user, :map, default: []
   prop selected_tab, :any, default: nil
   prop feed, :list, default: []
   prop page_info, :any, default: nil
