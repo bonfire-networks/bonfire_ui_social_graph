294199fc1fd17ebd8ae93b8d62443b06314841e5
Mayel de Borniol <git@mayel.space>
2025-03-13 15:36:29 +0000

    https://github.com/bonfire-networks/federated_archives/issues/6

--- config/bonfire_ui_social.exs
+++ config/bonfire_ui_social.exs
@@ -2,12 +2,3 @@ import Config
 
 config :bonfire_common,
   localisation_path: "priv/localisation"
-
-config :bonfire, :ui,
-  # used by ActivityLive - TODO: autogenerate?
-  verb_families: [
-    reply: ["Reply", "Respond"],
-    create: ["Create", "Write"],
-    react: ["Like", "Boost", "Flag", "Tag", "Pin"],
-    simple_action: ["Assign", "Label", "Schedule"]
-  ]
